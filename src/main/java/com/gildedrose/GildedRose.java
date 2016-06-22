4a5810c3c005351f5392d3aae6925e9e68a55b00
pravinhawale <pravinhawale@bitwiseglobal.com>
2016-06-22 02:32:17 -0500

    added new product - Sulfuras New

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,12 +9,16 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
+                    	items[i].quality = items[i].quality - 1;
+                    	if (items[i].name.equals("Sulfuras New") && items[i].quality < 10) {
+                    		items[i].quality = items[i].quality + 1;
+                    		 items[i].sellIn = items[i].sellIn - 1;
+                    		continue;   
+                    	}
+                    }                                     
                 }
             } else {
                 if (items[i].quality < 50) {
